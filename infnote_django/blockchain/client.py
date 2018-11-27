8e499c658bcd01afcba73c874e17289efc7d5a64
Vergil Choi <gameboy0824@gmail.com>
2018-11-27 13:01:50 +0800

    Amend

--- infnote_django/blockchain/client.py
+++ infnote_django/blockchain/client.py
@@ -24,18 +24,16 @@ class Client(metaclass=Singleton):
         self.create(self.user_chain, user, UserBlockchainSerializer)
 
     @staticmethod
-    def create(chain_id, obj, serializer) -> str:
+    def create(chain_id, data, serializer) -> str:
         with grpc.insecure_channel('localhost:32700') as channel:
             stub = BlockchainStub(channel)
-            s = serializer(data=obj)
-            if not s.is_valid():
-                raise ValueError('Invalid data with specific serializer.')
-            data = s.validated_data
             string = json.JSONEncoder(ensure_ascii=False, separators=(',', ':')).encode(data)
             response = stub.create_block(Payload(chain_id=chain_id, content=string))
             if response.chain_id is not None and len(response.chain_id) > 0:
-                obj.block_time = response.time
-                obj.block_height = response.height
-                obj.save()
+                data['block_time'] = response.time
+                data['block_height'] = response.height
+                s = serializer(data=data)
+                if s.is_valid():
+                    s.save()
                 return response.chain_id
             raise ValueError('Received a reponse without chain_id, there might be not the correct chain existed.')
